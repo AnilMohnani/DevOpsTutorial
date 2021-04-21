51a2524004a7715af224eaa5503fc3d370ad8499
Anil <mohnani.anil@gmail.com>
2021-04-22 02:01:18 +0530

    added selenium grid code

--- src/test/java/testCases/TestClassA.java
+++ src/test/java/testCases/TestClassA.java
@@ -1,10 +1,16 @@
 package testCases;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.sql.Driver;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -15,10 +21,13 @@ public class TestClassA {
 
 	WebDriver driver;
 	@BeforeMethod
-	public void login()
+	public void login() throws MalformedURLException
 	{
-		WebDriverManager.chromedriver().setup();
-		driver=new ChromeDriver();
+		DesiredCapabilities dr=DesiredCapabilities.chrome();
+		//dr.setPlatform(Platform.WIN10);
+		dr.setBrowserName("chrome");
+       System.setProperty("webdriver.chrome.driver", "C:/Users/AnilY/Downloads/chromedriver_win32/chromedriver.exe");
+       driver=new RemoteWebDriver(new URL("http://192.168.99.1:4444/wd/hub"), dr);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 	@Test
@@ -32,6 +41,14 @@ public class TestClassA {
 	public void test2()
 	{
 	 driver.get("https://cricbuzz.com/");
+	 System.out.println(driver.getTitle());
+		
+	}
+	
+	@Test
+	public void test3()
+	{
+	 driver.get("https://shaadi.com/");
 	 System.out.println(driver.getTitle());
 		
 	}
